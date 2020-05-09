445ed4fb194ac3a138bc97ad422c81f3ad475d1e
Santiago <santiagohernandez.1997@gmail.com>
2020-05-08 22:57:02 -0300

    Revert "Version 3"

--- version-3/build.sh
+++
@@ -1,3 +0,0 @@
-BASEDIR=$(dirname "$0")
-GO111MODULE=off swagger generate spec -o $BASEDIR/swagger/swagger.yml --scan-models 
-GO111MODULE=off swagger generate spec -o $BASEDIR/swagger/swagger.json --scan-models 
